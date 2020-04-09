0f41318733756904ed8ae5fb97d4e2f5032fa346
sts_amandeepkaur <sts.amandeepkaur@gmail.com>
2020-04-09 16:45:33 +0530

    Extract Model Data

--- secondapp/views.py
+++ secondapp/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from secondapp.models import Contact_Us
 
 def index(request):
-    return render(request,"home.html")
+    recent = Contact_Us.objects.all().order_by("-id")[:5]
+    print(recent)
+    return render(request,"home.html",{"messages":recent})
 
 def aboutpage(request):
     return render(request,"about.html")
 
 def contactpage(request):
-    return render(request,"contact.html")
+    all_data = Contact_Us.objects.all().order_by("-id")
+    if request.method=="POST":
+        nm = request.POST["name"]
+        con = request.POST["contact"]
+        sub = request.POST["subject"]
+        msz = request.POST["message"]
+
+        data = Contact_Us(name=nm,contact_number=con,subject=sub,message=msz)
+        data.save()
+        res = "Dear {} Thanks for your feedback".format(nm)
+        return render(request,"contact.html",{"status":res,"messages":all_data})
+        # return HttpResponse("<h1 style='color:green;'>Dear {} Data Saved Successfully!</h1>".format(nm))
+        
+
+    return render(request,"contact.html",{"messages":all_data})
 
